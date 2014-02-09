6c95808004d274b3e56ab2d5f4f3de083e787e4b
Jinto Jose <jintoppy@gmail.com>
2014-02-09 07:47:20 +0530

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  var meta = {
+    banner: '/*\n  <%= pkg.title || pkg.name %> <%= pkg.version %>' +
+      '<%= pkg.homepage ? " <" + pkg.homepage + ">" : "" %>' + '\n' +
+      '  Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>' +
+      '\n\n  Released under <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n*/\n',
+    pre: '\n(function(window, document, undefined){\n\n',
+    post: '\n})(window,document);'
+  };
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      dist: {
+        src: ['src/moviemaker.js'],
+        dest: 'src/moviemaker.<%= pkg.version %>.min.js'
+      },
+      options: {
+        banner: meta.banner
+      }
+    }
+  });
+
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  // Default task.
+  grunt.registerTask('default', ['uglify']);
+
+};
